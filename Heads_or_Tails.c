fbd99cf73d49a83e2e53f765f59a27205c369ef8
Take <naotake3704@gmail.com>
2020-08-06 16:01:34 +0900

    HoT: Initial revision

---
+++ Heads_or_Tails.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int c1,c2,c3=0;
+  int heads,tails=0;
+
+
+  srand(time(NULL));
+  c1 = rand() % 2 ;
+  c2 = rand() % 2 ;
+  c3 = rand() % 2 ;
+
+  printf("Tossing a coin...\n");
+  if(c1==0){
+  printf("Round 1:Heads\n");
+  heads++;
+}
+  else{
+    printf("Round 1:Tails\n");
+    tails++;
+  }
+  if(c2==0){
+  printf("Round 1:Heads\n");
+  heads++;
+}
+  else{
+    printf("Round 1:Tails\n");
+    tails++;
+  }
+  if(c3==0){
+  printf("Round 1:Heads\n");
+  heads++;
+}
+  else{
+    printf("Round 1:Tails\n");
+    tails++;
+  }
+   printf("Heads:%d,Tails:%d\n",heads,tails);
+
+  
+
+  return 0;
+}
